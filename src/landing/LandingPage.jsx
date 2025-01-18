e9094de8809174d2acb73774bbfb41bb147de5ae
Robert Zehnder <robertzehnder@Roberts-MacBook-Pro.local>
2025-01-18 18:43:24 -0500

    Fixes faulty styling with login modal - not enough padding on right side

--- src/landing/LandingPage.jsx
+++ src/landing/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./landingPage.css";
+import "./LandingPage.css";
 
 const LandingPage = () => {
   const navigate = useNavigate();
